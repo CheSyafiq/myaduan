dbb73dffaf244063c2125587c01a7a447e1ccb7f
C-M7S5 <65268872+CheSyafiq@users.noreply.github.com>
2020-06-27 15:19:25 +0800

    initail code-hi

---
+++ resources/views/main/main.blade.php
@@ -0,0 +1,164 @@
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Sistem Aduan Berpusat</title>
+    <!-- custom-theme -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Esteem Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript">
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- //custom-theme -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link href="css/component.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/export.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/flipclock.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/circles.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/style_grid.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/main.css" rel="stylesheet" type="text/css" media="all" />
+
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome-icons -->
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
+</head>
+
+<body>
+    <!-- banner -->
+    <div class="wthree_agile_admin_info">
+        <!-- /w3_agileits_top_nav-->
+        <!-- /nav-->
+        <div class="w3_agileits_top_nav">
+            <ul id="gn-menu" class="gn-menu-main">
+                <li class="second logo pull-left">
+                    <h1><a href="sistem_aduan_berpusat.html"><i class="fa fa-graduation-cap" aria-hidden="true"></i>POLITEKNIK SULTAN HAJI AHMAD SHAH</a></h1>
+                </li>
+            </ul>
+        </div>
+        <div style="margin-right: 20px;" class="inner_content_w3_agile_info">
+            <ul>
+                <div class="second logo pull-right">
+                    <h1>
+                        <a href="">
+                            <img style="width: 100px;height: 70px;margin-top: 10px;" src="/image/soaring_upwards.png" alt="">
+                        </a>
+                    </h1>
+                </div>
+                <div class="second logo pull-right">
+                    <h1>
+                        <a href="">
+                            <img style="width: 120px;height: 80px;margin-top: 10px;" src="/image/Logo%20KPT.png" alt="">
+                        </a>
+                    </h1>
+                </div>
+                <div class="second logo pull-right">
+                    <h1>
+                        <a href="">
+                            <img style="width: 200px;height: 80px;margin-top: 10px;" src="/image/NAMA%20POLISAS.png" alt="">
+                        </a>
+                    </h1>
+                </div>
+            </ul>
+        </div>
+        <div class="inner_content_w3_agile_info">
+            <div class="col-lg-12 button_set_one three agile_info_shadow">
+                <h3 style="color: Black" class="w3_inner_tittle text-center">Selamat Datang ke</h3>
+                <h3 style="color: Black" class="w3_inner_tittle text-center">Sistem Aduan Berpusat</h3>
+                <div class="col-lg-12 text-center">Jika anda seorang staff dan ingin membuat aduan, boleh pilih butang
+                    dibawah dan klik.</div>
+                <div class='row'>
+                    <div class='col-12'>
+                        <div class='row'>
+                            <div class="col-md-6">
+                                <div class="staff_elem">
+                                    <img src="/image/ict.jpg" class="staff_img">
+                                    <h1 class="staff_h1"
+                                        style="color: blue; margin-top: 50px;background:rgba(255,255,255,0.7)"><img src="https://png.icons8.com/dotty/70/000000/conference-background-selected.png">Staf</h1>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="staff_cate">
+                                    <div>
+                                        <button class="btn btn-danger"><img src="https://png.icons8.com/ios/50/000000/system-information-filled.png">Aduan ICT</button>
+                                    </div>
+                                    <div>
+                                        <button class="btn btn-primary"><img src="https://png.icons8.com/dotty/50/000000/services.png">Aduan Infra</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-12 text-center">Jika anda orang awam dan ingin membuat aduan, boleh klik butang
+                    dibawah.</div>
+                <div style="height: 130px;background: url('/image/people_to_count.jpg');"
+                    class="col-md-6 socail_grid_agile gmail">
+                    <div class="clearfix"></div>
+                    <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                        <h1 style="color: black;background:rgba(255,255,255,0.7)"><img
+                                src="https://png.icons8.com/ios/70/000000/crowd-filled.png"> Orang Awam</h1>
+                    </div>
+                    <div class="clearfix"></div>
+
+                </div>
+
+                <div style="height: 130px;" class="col-md-6 socail_grid_agile gmail">
+                    <div class="clearfix"></div>
+                    <a href="../user/sistemaduan.php">
+                        <div style="height: 80px;" class="col-md-12 col-sm-12 col-xs-12 text-center">
+                            <button style="color: white;background: #d12929;width:100%;height: 80px"><img
+                                    src="https://png.icons8.com/wired/70/000000/online-support.png"> Aduan
+                                Pelanggan</button>
+                        </div>
+                        <div class="clearfix"></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- //inner_content_w3_agile_info-->
+        <div style="height: 200px" class="clearfix"></div>
+        <footer>
+            <div class="container">
+                <?php $date=date('Y'); ?>
+                <div class="copy text-center">
+                    <strong>Copyright &copy; <?php echo $date; ?> <a href="#">POLITEKNIK SULTAN HAJI AHMAD
+                            SHAH</a>.</strong> All rights reserved.
+                </div>
+
+            </div>
+        </footer>
+    </div>
+    <!-- //inner_content-->
+
+    <!-- banner -->
+    <!--copy rights start here-->
+
+    <!--copy rights end here-->
+    <!-- js -->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
